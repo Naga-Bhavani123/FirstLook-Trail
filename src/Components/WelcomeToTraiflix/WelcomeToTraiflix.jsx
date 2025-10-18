4422142779612fa9feb9f039b05b6ff9420131c2
Yashoda <148939571+Naga-Bhavani123@users.noreply.github.com>
2025-10-18 11:14:14 +0530

    Update WelcomeToTraiflix.jsx

--- src/Components/WelcomeToTraiflix/WelcomeToTraiflix.jsx
+++ src/Components/WelcomeToTraiflix/WelcomeToTraiflix.jsx
@@ -4,7 +4,7 @@ import './WelcomeToTraiflix.css'
 const WelcomeToTraiflix = () => {
   return (
     <div className="redirectionTraiflix">
-              <h3 className="redirectTraiflixHead">Welcome To FirtLook, Please Wait...</h3>
+              <h3 className="redirectTraiflixHead">Welcome To FirstLook, Please Wait...</h3>
        </div>
   )
 }
